8090c891f76b1ea26301d414ee04f812fe33ba44
Denis <fortunmen228@mail.ru>
2024-07-16 15:53:36 +0500

    Delete migrations

--- TenzorQuizPr/main/models.py
+++ TenzorQuizPr/main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from teams.models import Team
 
 class User(models.Model):
     class Role(models.IntegerChoices):
