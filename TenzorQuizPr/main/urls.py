f98be52aab7f1198ae6991ce8878a1cada1afe47
Nesmiyanovvv <94855641+Nesmiyanovvv@users.noreply.github.com>
2024-07-17 12:35:13 +0500

    Update urls.py
    добавил api(регистрация, авторизация, просмотр двух токенов)

--- TenzorQuizPr/main/urls.py
+++ TenzorQuizPr/main/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import LoginUserApiView, CustomTokenObtainPairView, RegisterUserApiView
+from .views import RegisterView, CustomTokenObtainPairView, LoginView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path('api/v1/login/', LoginUserApiView.as_view()),
-    path('api/v1/token/', CustomTokenObtainPairView.as_view()),
-    path('api/v1/register/', RegisterUserApiView.as_view()),
+    path('api/v1/customtoken/', CustomTokenObtainPairView.as_view(), name='customtoken'),
+    path('api/v1/login/', LoginView.as_view(), name='login'),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/register/', RegisterView.as_view()),
 ]
