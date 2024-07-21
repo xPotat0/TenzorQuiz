0206f0e7080e811089a88a4b0d28463f8f966105
Nesmiyanovvv <94855641+Nesmiyanovvv@users.noreply.github.com>
2024-07-22 00:30:11 +0500

    Написал serializers.py

--- TenzorQuizPr/news/serializers.py
+++ TenzorQuizPr/news/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
-from news.models import News
+from django.contrib.auth import get_user_model
+from .models import News
+
+User = get_user_model()
 
 
 class NewsSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = News
         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    news = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'role', 'news']
