b5c773a986530fe30c66e7ff8ff7ddadd5020153
Denis <106258306+xPotat0@users.noreply.github.com>
2024-07-22 16:20:02 +0500

    Update models.py

--- TenzorQuizPr/news/models.py
+++ TenzorQuizPr/news/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from games.models import Game
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
 class News(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(blank=True, default='')
     publication_date = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, related_name='news', on_delete=models.CASCADE)
     image = models.ImageField(upload_to='news/%Y-%m-%d/', blank=True)
     game = models.OneToOneField(Game, on_delete=models.SET_NULL, null=True, blank=True)
 
