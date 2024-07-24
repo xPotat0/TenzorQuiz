3358d661cd68dca51fc37519bd7d45799a52a3b4
Nesmiyanovvv <94855641+Nesmiyanovvv@users.noreply.github.com>
2024-07-24 23:21:45 +0500

    Update serializers.py

--- TenzorQuizPr/news/serializers.py
+++ TenzorQuizPr/news/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from games.serializers import GamesSerializer
 from .models import News
+from games.models import Game
 
 User = get_user_model()
 
@@ -8,6 +10,7 @@ User = get_user_model()
 class NewsSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     image = serializers.ImageField(required=False, allow_null=True)
+    games = GamesSerializer(many=True, read_only=True)
 
     class Meta:
         model = News
@@ -20,3 +23,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'role', 'news']
+
+
+class GameForNewsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Game
+        fields = ['game_name', 'game_description', 'game_date']
