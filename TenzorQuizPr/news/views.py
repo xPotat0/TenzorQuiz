313a6030820cdb2bb213677b106774bee5d4ed69
Nesmiyanovvv <94855641+Nesmiyanovvv@users.noreply.github.com>
2024-07-24 16:12:01 +0500

    Update search

--- TenzorQuizPr/news/views.py
+++ TenzorQuizPr/news/views.py
@@ -6,6 +6,7 @@ from .models import News
 from .serializers import NewsSerializer
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework import filters
+from django.db.models import Q
 
 
 class IsLeading(permissions.BasePermission):
@@ -53,7 +54,12 @@ class NewsView(viewsets.ModelViewSet):
             page = 1
         page = int(page)
 
-        news = News.objects.filter(game_name__icontains=search).order_by(order).all()[(page - 1) * 10:page * 10]
+        words = search.strip().split() # можно разбить регуляркой
+        or_contains = Q()
+        for word in words:
+            or_contains |= Q(news_name__icontains=word)
+
+        news = News.objects.filter(or_contains).order_by(order).all()[(page-1)*10:page*10]
         content = NewsSerializer(news, many=True).data
         return super().list(request, content, *args, **kwargs)
 
