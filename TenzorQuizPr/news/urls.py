672b4d4c24f2a8947c15f1ce5f734f3b5b2cd9f7
Nesmiyanovvv <94855641+Nesmiyanovvv@users.noreply.github.com>
2024-07-24 23:22:23 +0500

    Update urls.py

--- TenzorQuizPr/news/urls.py
+++ TenzorQuizPr/news/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import NewsView
+from .views import NewsView, NewsAndScheduledGamesAPIView
 
 router = DefaultRouter()
 router.register(r'news', NewsView)
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
+    path('api/v1/news-and-scheduled-games/', NewsAndScheduledGamesAPIView.as_view(), name='news-and-scheduled-games'),
 ]
