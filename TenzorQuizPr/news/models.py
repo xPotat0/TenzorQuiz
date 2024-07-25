0c79687a9eabc48510ba544532bbca003cdb1d99
Nesmiyanovvv <94855641+Nesmiyanovvv@users.noreply.github.com>
2024-07-25 13:07:26 +0500

    Update news/models.py

--- TenzorQuizPr/news/models.py
+++ TenzorQuizPr/news/models.py
@@ -1,21 +1,13 @@
 from django.db import models
-from django.contrib.auth import get_user_model
-from games.models import Game
-
-
-User = get_user_model()
 
 
 class News(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(blank=True, default='')
-    publication_date = models.DateTimeField(auto_now_add=True)
-    owner = models.ForeignKey(User, related_name='news', on_delete=models.CASCADE)
     image = models.ImageField(upload_to='news/%Y-%m-%d/', blank=True)
-    games = models.ManyToManyField(Game, blank=True)
 
     class Meta:
-        ordering = ['-publication_date']
+        ordering = ['-id']
 
     def __str__(self):
         return self.title
