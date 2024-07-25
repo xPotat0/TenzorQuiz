5f8482630dc5b2a386824d07d8dae2481171072d
Nesmiyanovvv <94855641+Nesmiyanovvv@users.noreply.github.com>
2024-07-25 17:52:53 +0500

    Update games/urls.py

--- TenzorQuizPr/games/urls.py
+++ TenzorQuizPr/games/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from .views import ScheduledGamesAPIView
+from .views import ScheduledGamesAPIView, PlannedGameDetailAPIView
 
 urlpatterns = [
         path('api/v1/games/', views.GamesAPIView.as_view()),
@@ -10,4 +10,5 @@ urlpatterns = [
         path('api/v1/games/<int:game_id>/play/', views.PlayGameAPIView.as_view()),
         path('api/v1/games/<int:game_id>/teams/', views.GameAddTeamAPIView.as_view()),
         path('api/v1/scheduled-games/', ScheduledGamesAPIView.as_view(), name='scheduled-games'),
+        path('api/v1/planned-game/<int:id>/', PlannedGameDetailAPIView.as_view(), name='planned-game-detail'),
 ]
